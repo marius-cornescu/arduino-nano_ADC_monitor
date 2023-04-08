157c5a13d7ff595b8346e769f91f4385bf55900e
Marius Cornescu <marius_cornescu@yahoo.com>
2023-04-08 13:06:24 +0200

    Add voltage measurement and printing

--- nano_ADC_monitor.ino
+++ nano_ADC_monitor.ino
@@ -12,102 +12,135 @@
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define DEBUG
-
-#define ARRAY_LEN(array) (sizeof(array) / sizeof(array[0]))
+//#define UseIoT
 
 #define SEC 1000  // 1 second
 
 //= INCLUDES =======================================================================================
-#include "Secrets.h"
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
 
 //= CONSTANTS ======================================================================================
 const byte LED_INDICATOR_PIN = LED_BUILTIN;  // choose the pin for the LED // D13
 //------------------------------------------------
+const float OPERATING_VOLTAGE = 5.0;
+const byte ANALOG_PIN_COUNT = 8;    // Arduino NANO has 8 analog pins
+//
 #ifdef DEBUG
 const byte TIME_TICK = 100;
 #else
 const byte TIME_TICK = 10;
 #endif
 //------------------------------------------------
-const char host_name[] = HOST_NAME;
-
-// WiFi credentials (password to "" for open networks)
-const char ssid[] = WIFI_SSID;
-const char pass[] = WIFI_PASSWORD;
 
 //= VARIABLES ======================================================================================
-WiFiClient espClient;
-
-int analogPin = A0;  // connected to analog pin 0 - +3.3V
-int val = 0;         // variable to store the value read
+byte analog_pin[ANALOG_PIN_COUNT] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int voltage[ANALOG_PIN_COUNT];
 
 //##################################################################################################
 //==================================================================================================
 //**************************************************************************************************
 void setup() {
-  // Open serial communications and wait for port to open
-  Serial.begin(115200);
-  while (!Serial) { ; }
-  //..............................
 #ifdef DEBUG
-  Serial.println("Smart-PCM:Setup >>>");
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  Serial.println("START-UP >>>");
 #endif
   //..............................
   // initialize digital pin LED_INDICATOR_PIN as an output.
   pinMode(LED_INDICATOR_PIN, OUTPUT);
   //
-  pinMode(A0, INPUT);
-  //..............................
-  wifi_Setup();
-  //
-  mqtt_Setup();
+  adc_Setup();
   //..............................
 #ifdef DEBUG
-  Serial.println(">>> Smart-PCM:Setup");
+  Serial.println(">>> START-UP");
 #endif
 }
 //**************************************************************************************************
-void wifi_Setup() {
-  delay(TIME_TICK);
-  WiFi.hostname(host_name);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+void adc_Setup() {
+#ifdef DEBUG
+  Serial.println("NANO-ADC:Setup >>>");
+#endif
+  //..............................
+  for (byte pinId = 0; pinId < ANALOG_PIN_COUNT; pinId++) {
+    pinMode(analog_pin[pinId], INPUT);
+    voltage[pinId] = 0;
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  //..............................
+  iot_Setup();
+  //..............................
+#ifdef DEBUG
+  Serial.println(">>> NANO-ADC:Setup");
+#endif
 }
 //**************************************************************************************************
 //OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 void loop() {
   digitalWrite(LED_INDICATOR_PIN, LOW);
+  //..............................
   //
-  int x = analogRead(A0);
-  Serial.print("Analog value : ");
-  Serial.println(x);
+  _readAnalogPinData();
   //
+  _publishAnalogPinData();
+  //..............................
   digitalWrite(LED_INDICATOR_PIN, HIGH);
   //
   delay(10 * TIME_TICK);
 }
+//OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 //==================================================================================================
-bool shouldSendRfCode() {
-  return true;
+void _readAnalogPinData() {
+  for (byte pinId = 0; pinId < ANALOG_PIN_COUNT; pinId++) {
+    int raw_analog = analogRead(analog_pin[pinId]); // integer values between 0 and 1023
+    float measured_voltage = _computeVoltage(raw_analog);
+    pinMode(analog_pin[pinId], INPUT);
+    voltage[pinId] = _asMilliVolts(measured_voltage);
+  }
 }
 //==================================================================================================
-void transmitRfCode(unsigned long code, unsigned int code_length) {
+void _publishAnalogPinData() {
+#ifdef DEBUG
+  Serial.println("---------------------------------------");
+  for (byte pinId = 0; pinId < ANALOG_PIN_COUNT; pinId++) {
+    Serial.print("Pin [A");
+    Serial.print(pinId);
+    Serial.print("] = ");
+    Serial.print(voltage[pinId]);
+    Serial.println(" mV");
+  }
+  Serial.println("---------------------------------------");
+#endif
+}
+//==================================================================================================
+int _asMilliVolts(float measured_voltage) {
+  return round(measured_voltage * 1000);  // round to milli-volts
+}
+//==================================================================================================
+float _computeVoltage(int raw_analog_value) {
+  float raw_voltage = raw_analog_value * OPERATING_VOLTAGE / 1024.0;
+  // read correct supply voltage
+  float supply_voltage = _readVcc() / 1000.0;
+  float corrected_voltage = supply_voltage / OPERATING_VOLTAGE * raw_voltage;
+  return corrected_voltage;
+}
+//==================================================================================================
+long _readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Calculate Vcc (in mV); 1126400 = 1.1*1024*1000
+  return result;
 }
 //==================================================================================================
